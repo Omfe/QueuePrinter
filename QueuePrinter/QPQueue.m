49fb35ca5b4206c94d2a671761e268fc0c08aa6f
Omar Gudino <omfegumo6@gmail.com>
2012-10-21 21:33:21 -0700

    QPQueue methods done.

--- QueuePrinter/QPQueue.m
+++ QueuePrinter/QPQueue.m
@@ -10,19 +10,32 @@
 
 @implementation QPQueue
 
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.queueArray = [NSMutableArray array];
+    }
+    return self;
+}
+
+#pragma mark - Public Methods
 - (void)pushAnObject
 {
+    NSString *object;
     
+    object = [[NSString alloc]initWithFormat:@"%i", self.queueArray.count]; //Inicializar String
+    [self pushObject:object];
 }
 
 - (void)popAnObject
 {
-    
+    [self.queueArray removeObjectAtIndex:0];
 }
 
 - (void)pushObject:(NSString *)object
 {
-    
+    [self.queueArray addObject:object];
 }
 
 @end
